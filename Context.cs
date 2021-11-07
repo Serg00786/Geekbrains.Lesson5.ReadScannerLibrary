9aa36a99a394ef629d698b7f08cb5bb15ba410c3
Serg <a_s_v86@mail.ru>
2021-11-07 22:24:47 +0300

    Initial

---
+++ Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadScannerLibrary
+{
+    class Context
+    {
+
+        private IStrategy _strategy;
+
+        public Context()
+        { }
+        public Context(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        public void SetStrategy(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+    }
+}
---
+++ StrategySaveToJson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadScannerLibrary
+{
+    public class StrategySaveToJson : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
